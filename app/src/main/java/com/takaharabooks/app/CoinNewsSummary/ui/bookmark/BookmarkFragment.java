5e9a40596db2a16951e098f394b7b45a79d8f250
k0j1 <k0j123t@gmail.com>
2023-09-22 01:38:23 +0900

    AndroidStudioバージョン更新に伴う変更 ・CoinNewsSummaryとしてタイトル、パッケージ名変更 ・ビルド構成変更 ・JDK11→JDK17に変更 ・英語タイトルに書き換え
    Comment:
    ひとまず動作するところまで対応

---
+++ app/src/main/java/com/takaharabooks/app/CoinNewsSummary/ui/bookmark/BookmarkFragment.java
@@ -0,0 +1,111 @@
+package com.takaharabooks.app.CoinNewsSummary.ui.bookmark;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.takaharabooks.app.CoinNewsSummary.MainActivity;
+import com.takaharabooks.app.CoinNewsSummary.R;
+import com.takaharabooks.app.CoinNewsSummary.pref.DB_Data;
+import com.takaharabooks.app.CoinNewsSummary.ui.common.ListViewFunc;
+import com.takaharabooks.app.CoinNewsSummary.ui.item.RssItem;
+import com.takaharabooks.app.CoinNewsSummary.ui.item.RssItemListAdapter;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class BookmarkFragment extends Fragment {
+
+    private BookmarkViewModel bookmarkViewModel;
+    private View mRoot;
+    private AdView mAdView;
+
+    //protected List<RssItem> mItems = null;
+    protected RssItemListAdapter mAdapter;
+    private DB_Data m_dbData;
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main, menu);
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        setHasOptionsMenu(true);
+
+        bookmarkViewModel = new ViewModelProvider(requireActivity()).get(BookmarkViewModel.class);
+
+        mRoot = inflater.inflate(R.layout.fragment_bookmark, container, false);
+
+        m_dbData = new DB_Data(this.getContext());
+        m_dbData.InitDB();
+
+        // 初回RSSデータ読み込み
+        InitRssData();
+        //InitListView(mItems)
+        InitAd();
+
+        return mRoot;
+    }
+
+    /***********************************************************************************************
+     * リストの初期設定<br>
+     * 2chから取得したデータを表示
+     *
+     * @param	items
+     * @return	none
+     ***********************************************************************************************/
+    public void InitListView(List<RssItem> items)
+    {
+        ListView RssList = mRoot.findViewById(R.id.layout_rss_listview);
+        mAdapter = new RssItemListAdapter(this.getContext(), items, m_dbData);
+        // アダプタをリストビューにセットする
+        ListViewFunc.InitListView((MainActivity)getActivity(), m_dbData, items, RssList, mAdapter);
+    }
+
+    //
+    /***********************************************************************************************
+     * RSSデータ読み込み<br>
+     * ブックマークしたデータをDBから読み込んで表示
+     *
+     * @return	none
+     ***********************************************************************************************/
+    public void InitRssData()
+    {
+        bookmarkViewModel.clearItems();
+        bookmarkViewModel.getItems(m_dbData, this).observe(getViewLifecycleOwner(), items ->
+        {
+            // Update the UI.
+            //if(items != null && items.size() > 0)
+            //{
+            //    //InitListView(items);
+            //}
+        });
+    }
+
+    /*************************************
+     * 広告の初期設定
+     **************************************/
+    public void InitAd()
+    {
+        MobileAds.initialize(getContext(), initializationStatus -> {
+
+        });
+        mAdView = mRoot.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+}
